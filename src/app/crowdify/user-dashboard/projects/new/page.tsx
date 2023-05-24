74e0b9011b515b03b72beed288456cbf3a508f76
mambo-dev <mambo.michael.22@gmail.com>
2023-05-24 07:07:27 +0300

    add: project stepper component

--- src/app/crowdify/user-dashboard/projects/new/page.tsx
+++ src/app/crowdify/user-dashboard/projects/new/page.tsx
@@ -1,13 +1,58 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import ProjectStepper from "../../../../../components/projects/project-stepper";
+import CreateFundraiser from "../../../../../components/projects/create-fundraiser";
+import AddImages from "../../../../../components/projects/add-images";
+import CreateProject from "../../../../../components/projects/create-project";
 
 type Props = {};
 
 const CreateProjectPage = (props: Props) => {
+  const [currentStep, setCurrentStep] = useState({
+    step: 1,
+    text: "Create project",
+    complete: false,
+  });
+  const [steps, setSteps] = useState<
+    { step: number; text: string; complete: boolean }[]
+  >([
+    {
+      step: 1,
+      text: "Create project",
+      complete: false,
+    },
+    {
+      step: 2,
+      text: "Add goal",
+      complete: false,
+    },
+    {
+      step: 3,
+      text: "Upload images",
+      complete: false,
+    },
+  ]);
+
   //project
   //fundraiser
   //images
-  return <div>CreateProjectPage</div>;
+
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <ProjectStepper steps={steps} currentStep={currentStep} />
+      {currentStep.step === 1 ? (
+        <CreateProject
+          setCurrentStep={setCurrentStep}
+          setSteps={setSteps}
+          steps={steps}
+        />
+      ) : currentStep.step === 2 ? (
+        <CreateFundraiser />
+      ) : (
+        <AddImages />
+      )}
+    </div>
+  );
 };
 
 export default CreateProjectPage;
--- src/components/projects/create-project.tsx
+++ src/components/projects/create-project.tsx
@@ -2,10 +2,53 @@
 
 import React from "react";
 
-type Props = {};
+type Props = {
+  steps: { step: number; text: string; complete: boolean }[];
+  setCurrentStep: React.Dispatch<
+    React.SetStateAction<{
+      step: number;
+      text: string;
+      complete: boolean;
+    }>
+  >;
+  setSteps: React.Dispatch<
+    React.SetStateAction<
+      {
+        step: number;
+        text: string;
+        complete: boolean;
+      }[]
+    >
+  >;
+};
 
-const CreateProject = ({}: Props) => {
-  return <div>NewProject</div>;
+const CreateProject = ({ steps, setCurrentStep, setSteps }: Props) => {
+  return (
+    <div className="">
+      <button
+        onClick={() => {
+          const filterSteps = steps.filter((step) => step.step !== 1);
+
+          setSteps([
+            {
+              step: 1,
+              text: "Create project",
+              complete: true,
+            },
+            ...filterSteps,
+          ]);
+
+          setCurrentStep({
+            step: 2,
+            text: "Add goal",
+            complete: false,
+          });
+        }}
+      >
+        save
+      </button>
+    </div>
+  );
 };
 
 export default CreateProject;
